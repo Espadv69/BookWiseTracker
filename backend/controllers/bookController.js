9001c579e191eddcc633316af2caa42b54a03d62
Espadv69 <aitorjob69@gmail.com>
2025-04-03 16:59:20 +0200

    Add getBooks function to retrieve all books from the database

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,1 +1,12 @@
 import BOOK_MODEL from '../models/Book.js'
+
+// Get all books 📚
+const getBooks = async (req, res) => {
+  try {
+    // Fetch all books from the database
+    const books = await BOOK_MODEL.find()
+
+    // Send books as JSON response
+    res.status(200).json(books)
+  } catch (err) {}
+}
