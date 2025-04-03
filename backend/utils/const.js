56f7025c59276ae6e98f7556f7ae50e0f87b0f30
Espadv69 <aitorjob69@gmail.com>
2025-04-03 17:30:59 +0200

    Update BOOKS_ID_ROUTE to include dynamic book ID parameter

--- backend/utils/const.js
+++ backend/utils/const.js
@@ -1,4 +1,4 @@
 export const HOME_ROUTE = '/'
 export const API_ROUTE = '/api'
 export const BOOKS_ROUTE = '/books'
-export const BOOKS_ID_ROUTE = '/api/books'
+export const BOOKS_ID_ROUTE = '/books/:id'
