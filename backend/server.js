06ed5e746450fa945a45d2694d2479eb7cb615c7
Espadv69 <aitorjob69@gmail.com>
2025-04-03 14:49:03 +0200

    Configure dotenv and set up express middleware

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,14 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+
+// Dotenv configuration
+// Load environment variables from .env file
+dotenv.config()
+
+// Express app configuration
+const app = express()
+
+// Middleware configuration
+app.use(cors())
+app.use(express.json())
