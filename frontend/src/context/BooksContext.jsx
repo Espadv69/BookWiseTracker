844c69f28eb1832b32b2416651418964c5c052ae
Espadv69 <aitorjob69@gmail.com>
2025-04-04 13:37:45 +0200

    Set up BooksContext with useReducer and import AXIOS_API_URL for API requests

--- frontend/src/context/BooksContext.jsx
+++ frontend/src/context/BooksContext.jsx
@@ -2,9 +2,19 @@ import axios from 'axios'
 
 import { createContext, useReducer, useEffect } from 'react'
 import { BooksReducer } from './BooksReducer.js'
+import { AXIOS_API_URL } from '../utils/const.js'
 import {
   LOAD_BOOKS,
   ADD_BOOK,
   UPDATE_BOOK,
   DELETE_BOOK,
 } from './actionTypes.js'
+
+export const BooksContext = createContext()
+
+export const BooksProvider = ({ children }) => {
+  const [books, dispatch] = useReducer(BooksReducer, [])
+
+  // Load books from the server when the component mounts
+  useEffect(() => {}, [])
+}
