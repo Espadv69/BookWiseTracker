8223cf9ceb0ac0754f576ad510ebc9bd9536b290
Espadv69 <aitorjob69@gmail.com>
2025-04-04 13:05:14 +0200

    Add NavBar component with navigation links

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,24 @@
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 
-const NavBar = () => {}
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <h3>BookWise 🧙</h3>
+
+      <div className="links">
+        <Link to="/" className="nav__link">
+          Home
+        </Link>
+        <Link to="/add" className="nav__link">
+          Add Book
+        </Link>
+        <Link to="/details" className="nav__link">
+          Book Details
+        </Link>
+      </div>
+    </nav>
+  )
+}
 
 export default NavBar
