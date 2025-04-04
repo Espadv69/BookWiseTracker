9aea5d5b18b9ca2b7f766a7fadb01d8341ca212b
Espadv69 <aitorjob69@gmail.com>
2025-04-04 19:09:23 +0200

    Wrap App component with BooksProvider for context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BooksProvider } from './context/BooksContext.jsx'
 
 import App from './App.jsx'
 import './index.css'
@@ -8,6 +9,8 @@ const root = createRoot(document.getElementById('root'))
 
 root.render(
   <StrictMode>
-    <App />
+    <BooksProvider>
+      <App />
+    </BooksProvider>
   </StrictMode>,
 )
