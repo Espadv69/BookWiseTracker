73713aad3184935b33b9d3911049c54d1031ab27
Espadv69 <aitorjob69@gmail.com>
2025-04-04 18:38:09 +0200

    Add state management for form data in AddBook component

--- frontend/src/components/AddBook/AddBook.jsx
+++ frontend/src/components/AddBook/AddBook.jsx
@@ -7,7 +7,17 @@ import { AXIOS_API_URL } from '../../utils/const.js'
 import './AddBook.css'
 
 const AddBook = () => {
-  return <h2>Hello</h2>
+  // Context
+  const { dispatch } = useContext(BooksContext)
+
+  // State to manage form data
+  const [formData, setFormData] = useState({
+    title: '',
+    author: '',
+    coverImage: '',
+    totalPages: '',
+    currentPage: '',
+  })
 }
 
 export default AddBook
