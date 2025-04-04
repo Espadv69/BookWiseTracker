92c907cf4cb898e36d6b0860c4843bceac830552
Espadv69 <aitorjob69@gmail.com>
2025-04-04 13:00:53 +0200

    Implement routing in App component with placeholder routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,17 @@
-const App = () => {}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      {/* ToDo: Add NavBar */}
+      <Routes>
+        <Route path="/" element />
+        <Route path="/add" element />
+        <Route path="/details" element />
+        <Route path="*" element />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
