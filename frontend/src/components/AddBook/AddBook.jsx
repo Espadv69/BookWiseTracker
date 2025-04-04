f952a3347c3794dcf5f96476b237c5168622fcf4
Espadv69 <aitorjob69@gmail.com>
2025-04-04 18:35:38 +0200

    Add initial implementation of AddBook component with basic structure

--- frontend/src/components/AddBook/AddBook.jsx
+++ frontend/src/components/AddBook/AddBook.jsx
@@ -1,5 +1,13 @@
+import { useState, useContext } from 'react'
+import axios from 'axios'
+import { BooksContext } from '../../context/BooksContext'
+import { ADD_BOOK } from '../../context/actionTypes.js'
+import { AXIOS_API_URL } from '../../utils/const.js'
+
 import './AddBook.css'
 
-const AddBook = () => {}
+const AddBook = () => {
+  return <h2>Hello</h2>
+}
 
 export default AddBook
