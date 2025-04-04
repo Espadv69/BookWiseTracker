ebacd9b1836116272af5262ea2710c959012493a
Espadv69 <aitorjob69@gmail.com>
2025-04-04 13:23:31 +0200

    Implement BooksReducer to handle book management actions: LOAD_BOOKS, ADD_BOOK, UPDATE_BOOK, DELETE_BOOK

--- frontend/src/context/BooksReducer.js
+++ frontend/src/context/BooksReducer.js
@@ -4,3 +4,23 @@ import {
   UPDATE_BOOK,
   DELETE_BOOK,
 } from './actionTypes.js'
+
+export const BooksReducer = (state, action) => {
+  switch (action.type) {
+    case LOAD_BOOKS:
+      return action.payload
+
+    case ADD_BOOK:
+      return [action.payload, ...state]
+
+    case UPDATE_BOOK:
+      return state.map((book) =>
+        book._id === action.payload.id
+          ? { ...book, currentPage: action.payload.currentPage }
+          : book,
+      )
+
+    case DELETE_BOOK:
+      return state.filter((book) => book._id !== action.payload)
+  }
+}
