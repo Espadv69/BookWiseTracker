a3f69a12f6b75f056b6bb08b1084bb8b91bd446d
Espadv69 <aitorjob69@gmail.com>
2025-04-05 12:44:50 +0200

    Update default values for author and coverImage in bookSchema

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  author: { type: String, default: 'Unknown' },
-  coverImage: { type: String, default: 'No cover image' },
+  author: { type: String, default: '' },
+  coverImage: { type: String, default: '' },
   totalPages: { type: Number, required: true },
   currentPage: { type: Number, default: 0 },
   progress: { type: Number, default: 0 },
