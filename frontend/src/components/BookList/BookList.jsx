6f9bd6282c09b96ffe18246aabe98cd8beefe5f0
Espadv69 <aitorjob69@gmail.com>
2025-04-05 13:09:21 +0200

    Implement BookList component to display a list of books

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,5 +1,27 @@
+import BookCard from '../BookCard/BookCard'
 import './BookList.css'
 
-const BookList = () => {}
+const BookList = ({ books }) => {
+  if (!books.length) {
+    return <p className="book-list__empty">No books added yet.</p>
+  }
+
+  return (
+    <section className="book-list">
+      {books.map((book) => (
+        <BookCard
+          key={book._id}
+          title={book.title}
+          author={book.author}
+          coverImage={book.coverImage}
+          totalPages={book.totalPages}
+          currentPage={book.currentPage}
+          progress={book.progress}
+          status={book.status}
+        />
+      ))}
+    </section>
+  )
+}
 
 export default BookList
