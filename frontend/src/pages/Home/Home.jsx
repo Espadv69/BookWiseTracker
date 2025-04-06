c739ec52cf5790cd1af561e46af69e01a89aab0d
Espadv69 <aitorjob69@gmail.com>
2025-04-06 19:10:03 +0200

    Fix: Import necessary components and context in Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react'
+import { BooksContext } from '../../context/BooksContext'
+
+import BookCard from '../../components/BookCard/BookCard'
 import './Home.css'
 
 const Home = () => {}
