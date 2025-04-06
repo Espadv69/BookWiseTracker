777e30333516cb7202958f72a212e80e554600f9
Espadv69 <aitorjob69@gmail.com>
2025-04-06 19:18:56 +0200

    Fix: Remove unused BookCard import in Home component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react'
 import { BooksContext } from '../../context/BooksContext'
 
-import BookCard from '../../components/BookCard/BookCard'
 import './Home.css'
 
 const Home = () => {
